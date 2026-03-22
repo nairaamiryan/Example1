9933e7cf9764c9707d5162e30fc025ade70d084f
Vahram Ghazaryan <ghazaryanvahram8@gmail.com>
2026-03-22 17:28:33 +0400

    Added initial structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to React App</h1>
+            <p>This is a simple React application.</p>
+        </div>
+    );
+}
+
+export default App;
