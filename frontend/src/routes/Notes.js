112b2adc4c961dc93a9a13ef69d9a0004558a901
Naira <nairaamiryan5@gmail.com>
2026-04-19 19:42:10 +0400

    Update Notes.js

--- frontend/src/routes/Notes.js
+++ frontend/src/routes/Notes.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import api from "../services/api";
-import MedicalNote from "../components/MedicalNodes";
+import MedicalNote from "../components/MedicalNotes";
 
 const Notes = () => {
     const [notes, setNotes] = useState([]);
