572f7a8a1f7c60bf74176cea8dcf297bdaece496
Naira <nairaamiryan5@gmail.com>
2026-04-28 14:32:00 +0400

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import patientRoutes from "./routes/patientRoutes.js";
 import doctorRoutes from "./routes/doctorRoutes.js";
 import reportRoutes from "./routes/reportRoutes.js";
+import noteRoutes from "./routes/noteRoutes.js";
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(express.json());
 app.use("/api/patients", patientRoutes);
 app.use("/api/doctors", doctorRoutes);
 app.use("/api/reports", reportRoutes);
+app.use("/api/notes", noteRoutes);
 
 export default app;
