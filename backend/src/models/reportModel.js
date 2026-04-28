4442fd09086a726f96d2619d3e104ae2d9eced5f
Naira <nairaamiryan5@gmail.com>
2026-04-28 14:05:02 +0400

    Update reportModel.js

--- backend/src/models/reportModel.js
+++ backend/src/models/reportModel.js
@@ -1,1 +1,30 @@
+export default (sequelize, DataTypes) => {
+    const Report = sequelize.define(
+        "Report",
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            title: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            description: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            },
+            date: {
+                type: DataTypes.DATEONLY,
+                allowNull: false,
+            },
+        },
+        {
+            tableName: "reports",
+            timestamps: true,
+        },
+    );
 
+    return Report;
+};
