4806bb0729fcb8db413a571fe8e478f9b24a8542
Naira <nairaamiryan5@gmail.com>
2026-04-30 21:46:19 +0400

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
-import app from "./app.js";
-import db from "./models/index.js";
-import "./config/env.js";
+const app = require("./app");
+const db = require("./models/index");
+require("./config/env");
 
 const PORT = process.env.PORT || 5001;
 
@@ -8,9 +8,7 @@ const startServer = async () => {
     try {
         await db.sequelize.authenticate();
         console.log("Database connected");
-
         await db.sequelize.sync();
-
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`);
         });
