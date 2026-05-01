c1d92355eecabad3058d7c632cc60a30f1fd9d5a
Naira <nairaamiryan5@gmail.com>
2026-05-01 11:07:10 +0400

    Update reportRoutes.js

--- backend/src/routes/reportRoutes.js
+++ backend/src/routes/reportRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getReports, addReport } = require("../controllers/reportController");
+const { getReports, addReport, updateReport, deleteReport } = require("../controllers/reportController");
 
 const router = express.Router();
 
 router.get("/", getReports);
 router.post("/", addReport);
+router.put("/:id", updateReport);
+router.delete("/:id", deleteReport);
 
 module.exports = router;
