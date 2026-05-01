a6817d86e2168228d8f5dc5a49cd4d9d8a6822fd
Naira <nairaamiryan5@gmail.com>
2026-05-01 11:07:53 +0400

    Update noteRoutes.js

--- backend/src/routes/noteRoutes.js
+++ backend/src/routes/noteRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getNotes, addNote } = require("../controllers/noteController");
+const { getNotes, addNote, updateNote, deleteNote } = require("../controllers/noteController");
 
 const router = express.Router();
 
 router.get("/", getNotes);
 router.post("/", addNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
 
 module.exports = router;
