d1fe2a9a2deb73375fd1ed74667ea9cd6caf3115
Naira <nairaamiryan5@gmail.com>
2026-05-01 11:06:09 +0400

    Update patientRoutes.js

--- backend/src/routes/patientRoutes.js
+++ backend/src/routes/patientRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getPatients, createPatient } = require("../controllers/patientController");
+const { getPatients, createPatient, updatePatient, deletePatient } = require("../controllers/patientController");
 
 const router = express.Router();
 
 router.get("/", getPatients);
 router.post("/", createPatient);
+router.put("/:id", updatePatient);
+router.delete("/:id", deletePatient);
 
 module.exports = router;
