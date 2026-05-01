971566c5d84357ca4d70edfb02f6cc83e119765d
Naira <nairaamiryan5@gmail.com>
2026-05-01 11:08:30 +0400

    Update financeRoutes.js

--- backend/src/routes/financeRoutes.js
+++ backend/src/routes/financeRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getFinances, addFinance } = require("../controllers/financeController");
+const { getFinances, addFinance, updateFinance, deleteFinance } = require("../controllers/financeController");
 
 const router = express.Router();
 
 router.get("/", getFinances);
 router.post("/", addFinance);
+router.put("/:id", updateFinance);
+router.delete("/:id", deleteFinance);
 
 module.exports = router;
