5f75af1930f0c302aec744a3d0acdeb232701821
Naira <nairaamiryan5@gmail.com>
2026-05-03 16:48:10 +0400

    Update notificationRoutes.js

--- backend/src/routes/notificationRoutes.js
+++ backend/src/routes/notificationRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { getNotifications, deleteNotification } = require("../controllers/notificationController");
-
+const { getNotifications, updateNotification, deleteNotification } = require("../controllers/notificationController");
 const router = express.Router();
 
 router.get("/", getNotifications);
+router.put("/:id", updateNotification);
 router.delete("/:id", deleteNotification);
 
 module.exports = router;
