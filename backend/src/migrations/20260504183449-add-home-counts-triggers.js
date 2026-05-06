10eb47dde26b6d030becc2ff66d9881cb887fd16
Vahram Ghazaryan <ghazaryanvahram8@gmail.com>
2026-05-06 13:37:22 +0400

    Fix database connection by switching to separate env variables

--- backend/src/migrations/20260504183449-add-home-counts-triggers.js
+++ backend/src/migrations/20260504183449-add-home-counts-triggers.js
@@ -2,28 +2,36 @@
 
 const fs = require("fs");
 const path = require("path");
-
+const { Client } = require("pg");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
+        const client = new Client({
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+        });
+        await client.connect();
         const sql = fs.readFileSync(
             path.join(__dirname, "sql/update_statistics.sql"),
             "utf8"
         );
-        await queryInterface.sequelize.query(sql);
-        await queryInterface.sequelize.query(`
+        await client.query(sql);
+        await client.query(`
             CREATE TRIGGER patients_after_insert
             AFTER INSERT ON patients
             FOR EACH STATEMENT
             EXECUTE FUNCTION update_statistics();
         `);
-        await queryInterface.sequelize.query(`
+        await client.query(`
             CREATE TRIGGER patients_after_delete
             AFTER DELETE ON patients
             FOR EACH STATEMENT
             EXECUTE FUNCTION update_statistics();
         `);
-        await queryInterface.sequelize.query(`
+        await client.query(`
             CREATE TRIGGER doctors_after_insert
             AFTER INSERT ON doctors
             FOR EACH STATEMENT
@@ -31,48 +39,58 @@ module.exports = {
 
         `);
 
-        await queryInterface.sequelize.query(`
+        await client.query(`
             CREATE TRIGGER doctors_after_delete
             AFTER DELETE ON doctors
             FOR EACH STATEMENT
             EXECUTE FUNCTION update_statistics();
         `);
-        await queryInterface.sequelize.query(`
+        await client.query(`
             CREATE TRIGGER notifications_after_insert
             AFTER INSERT ON notifications
             FOR EACH ROW
             EXECUTE FUNCTION update_statistics();
         `);
 
-        await queryInterface.sequelize.query(`
+        await client.query(`
             CREATE TRIGGER notifications_after_delete
             AFTER DELETE ON notifications
             FOR EACH ROW
             EXECUTE FUNCTION update_statistics();
         `);
+        await client.end();
     },
 
     async down(queryInterface, Sequelize) {
-        await queryInterface.sequelize.query(
+        const client = new Client({
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+        });
+        await client.connect();
+        await client.query(
             `DROP TRIGGER IF EXISTS patients_after_insert ON patients;`,
         );
-        await queryInterface.sequelize.query(
+        await client.query(
             `DROP TRIGGER IF EXISTS patients_after_delete ON patients;`,
         );
-        await queryInterface.sequelize.query(
+        await client.query(
             `DROP TRIGGER IF EXISTS doctors_after_insert ON doctors;`,
         );
-        await queryInterface.sequelize.query(
+        await client.query(
             `DROP TRIGGER IF EXISTS doctors_after_delete ON doctors;`,
         );
-        await queryInterface.sequelize.query(
+        await client.query(
             `DROP TRIGGER IF EXISTS notifications_after_insert ON notifications;`,
         );
-        await queryInterface.sequelize.query(
+        await client.query(
             `DROP TRIGGER IF EXISTS notifications_after_delete ON notifications;`,
         );
-        await queryInterface.sequelize.query(
+        await client.query(
             `DROP FUNCTION IF EXISTS update_statistics;`,
         );
+        await client.end();
     },
 };
