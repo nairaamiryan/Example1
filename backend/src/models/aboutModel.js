df7d682833bbe2532f271c5ef4ace467efeafc2c
Naira <nairaamiryan5@gmail.com>
2026-05-06 09:04:16 +0400

    Create aboutModel.js

---
+++ backend/src/models/aboutModel.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+    const About = sequelize.define(
+        "About",
+        {
+            id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+            title: { type: DataTypes.STRING, allowNull: false },
+            description: { type: DataTypes.TEXT, allowNull: false },
+            founded: { type: DataTypes.STRING, allowNull: false },
+        },
+        { tableName: "abouts", timestamps: true }
+    );
+    return About;
+};
