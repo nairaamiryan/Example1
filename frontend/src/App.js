4dd22bf127fbf0eff830c9c42b075fc7696eb6bd
Naira <nairaamiryan5@gmail.com>
2026-05-09 13:37:00 +0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-import { NAVBAR } from "./constants";
+import { NAVBAR } from "../constants";
 import api from "../services/api";
 
 const FICTIVE_UNREAD_COUNT = 3;
