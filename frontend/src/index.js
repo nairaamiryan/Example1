e404499d5daa34c4f0fcc070b7e34b869d192ea8
Naira <nairaamiryan5@gmail.com>
2026-05-09 19:30:07 +0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,8 @@ import App from "./App";
 
 ReactDOM.render(
     <Auth0Provider
-        domain={process.env.REACT_APP_AUTH0_DOMAIN}
-        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+        domain="dev-euxwfhw80ges67ji.au.auth0.com"
+        clientId="GU55pj3yUwAZ5GIpcb5I3IAEBloSspV0"
         authorizationParams={{
             redirect_uri: window.location.origin
         }}
