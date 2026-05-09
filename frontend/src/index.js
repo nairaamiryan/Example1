b6f095d8a67cb5b60b796c1f7a0f602f154b8f8c
Naira <nairaamiryan5@gmail.com>
2026-05-09 19:14:42 +0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,11 @@ ReactDOM.render(
     <Auth0Provider
         domain={process.env.REACT_APP_AUTH0_DOMAIN}
         clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-        redirectUri={window.location.origin}
+        authorizationParams={{
+            redirect_uri: window.location.origin
+        }}
+        cacheLocation="localstorage"
+        useRefreshTokens={true}
     >
         <App />
     </Auth0Provider>,
