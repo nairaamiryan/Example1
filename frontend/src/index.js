208484f54e815ae799f127896b17543620bbad81
Naira <nairaamiryan5@gmail.com>
2026-05-09 13:05:53 +0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Auth0Provider
+        domain="dev-euxwfhw80ges67ji.au.auth0.com"
+        clientId="ՔՈ_CLIENT_ID_ԱՅՍՏԵՂ"
+        authorizationParams={{
+            redirect_uri: window.location.origin
+        }}
+    >
+        <App />
+    </Auth0Provider>,
+    document.getElementById("root")
+);
