eea00bb8c270895a54e4b9c18697792f5774fecc
Naira <nairaamiryan5@gmail.com>
2026-05-10 16:04:46 +0400

    Update 20260510120930-change-patient-id-to-passport.js

--- backend/src/migrations/20260510120930-change-patient-id-to-passport.js
+++ backend/src/migrations/20260510120930-change-patient-id-to-passport.js
@@ -2,12 +2,24 @@
 module.exports = {
     async up(queryInterface, Sequelize) {
         await queryInterface.sequelize.query(
+            `ALTER TABLE patients DROP CONSTRAINT patients_pkey;`
+        );
+        await queryInterface.sequelize.query(
             `ALTER TABLE patients ALTER COLUMN id TYPE VARCHAR(255);`
+        );
+        await queryInterface.sequelize.query(
+            `ALTER TABLE patients ADD PRIMARY KEY (id);`
         );
     },
     async down(queryInterface, Sequelize) {
         await queryInterface.sequelize.query(
+            `ALTER TABLE patients DROP CONSTRAINT patients_pkey;`
+        );
+        await queryInterface.sequelize.query(
             `ALTER TABLE patients ALTER COLUMN id TYPE INTEGER USING id::INTEGER;`
+        );
+        await queryInterface.sequelize.query(
+            `ALTER TABLE patients ADD PRIMARY KEY (id);`
         );
     },
 };
